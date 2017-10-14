419cbd53cd0b378c75d5439946f4005caf7268a6
lijuncheng <lijuncheng@u51.com>
2017-10-14 15:29:02 +0800

    html2canvas

--- src/components/Agent.vue
+++ src/components/Agent.vue
@@ -10,6 +10,8 @@
 import { Group, Cell } from 'vux'
 import qr from '../common/qr'
 import api from '../common/Request'
+import html2canvas from 'html2canvas'
+
 export default {
   components: {
     Group,
@@ -24,17 +26,39 @@ export default {
     let res = await api.getQr();
     let qrcode = res.data
     let ele = qr.makeImage(qrcode)
-    this.qrcode = ele.src
+    this.qrcode =ele.src
+  },
+  mounted(){
+    this.$nextTick(()=>{
+      document.querySelector('#qr-canvas').remove()
+      html2canvas(document.querySelector('.main-content')).then(canvas=>{
+        canvas.id = 'capture'
+        document.body.appendChild(canvas)
+        let imgData=  canvas.toDataURL('image/png')
+        let img = document.createElement('img')
+        img.id="capimg"
+        img.src=imgData
+        document.body.appendChild(img)
+      })
+    })
+    
   }
 }
 </script>
 
 <style>
+  #capimg{
+    position: absolute;
+    z-index: 9999;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom:0;
+  }
   .agent-bg{
     height: 100%;
     background: url(../assets/agent-bg.jpg) center no-repeat;
     background-size: 100% 100%;
-
   }
   .qrcode-fg,#qr-canvas{
     width: 180px;
